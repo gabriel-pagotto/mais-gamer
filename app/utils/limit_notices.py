80b42c3a40b3a483f6fd461472c46c734643b247
gabriel-pagotto <gabriel.pagotto@icloud.com>
2020-07-02 18:28:20 -0300

    Adiciona limite no index

--- app/utils/limit_notices.py
+++ app/utils/limit_notices.py
@@ -3,6 +3,8 @@ def limit_notices(game_id, notices):
     for notice in notices:
         if notice.game_id == game_id:
             necessary_notices.append(notice)
+    if len(necessary_notices) < 3:
+        return None
     if len(necessary_notices) >= 3:
         return [necessary_notices[0], necessary_notices[1], necessary_notices[2]]
     return necessary_notices
