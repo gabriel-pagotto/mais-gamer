b0b20b08995863a65763382e76dd65f05cc260df
gabriel-pagotto <gabriel.pagotto@icloud.com>
2020-07-16 18:52:05 -0300

    Adiciona HTTPS

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from app import app
 from app.models import Users, Posts, Games
 from config import Config
 
+debug = bool(os.environ.get('FLASK_DEBUG') == 'True')
 port = int(os.environ.get('PORT', 7000))
 
-app.run(host='0.0.0.0', port= port, debug=bool(os.environ.get('FLASK_DEBUG') == 'True'))
+app.run(host='0.0.0.0', port= port, debug=debug, ssl_context='adhoc')
 
 @app.shell_context_processor
 def make_shell_context():
