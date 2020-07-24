25e0fba9fa3e4605db41d0bf92dc154f3a86899c
gabriel-pagotto <gabriel.pagotto@icloud.com>
2020-07-24 13:46:48 -0300

    Adiciona função get_datetime_7_days

--- app/routes/cover.py
+++ app/routes/cover.py
@@ -1,7 +1,7 @@
 import os
 from app import app
 from app.models import Posts, Games
-from app.utils.date_time import DatePost
+from app.utils.date_time import DatePost, get_datetime_7_days
 from app.utils.header_games import header_games
 from app.utils.limit_notices import limit_notices
 from flask import render_template, redirect, flash, request, url_for
@@ -20,6 +20,7 @@ def index():
         ) 
     last_notices = []
     for post in posts:
+        print(get_datetime_7_days(post.addedAt))
         last_notices.append(post)
     last_notices.reverse()
     last_notice = last_notices[0]
--- app/utils/date_time.py
+++ app/utils/date_time.py
@@ -7,35 +7,35 @@ def DatePost(date_before):
 
     date_init = datetime(year=date_before.year, month=date_before.month, day=date_before.day)
     date_final = datetime(year=date_now.year, month=date_now.month, day=date_now.day, hour=date_now.hour)
-    diference_date = (date_final - date_init)
+    difference_date = (date_final - date_init)
 
-    if diference_date.days == 0:
+    if difference_date.days == 0:
         time_init = timedelta(hours=date_before.hour, minutes=date_before.minute, seconds=date_before.second)
         time_final = timedelta(hours=date_now.hour, minutes=date_now.minute, seconds=date_now.second)
-        diference_time = str(time_final - time_init).split(':')
-        diference_hour = int(diference_time[0])
-        diference_minute = int(diference_time[1])
-        diference_second = int(diference_time[2])
-        if diference_hour != 0:
-            if diference_hour == 1:
-                return str(diference_hour) + ' hora atrás'
+        difference_time = str(time_final - time_init).split(':')
+        difference_hour = int(difference_time[0])
+        difference_minute = int(difference_time[1])
+        difference_second = int(difference_time[2])
+        if difference_hour != 0:
+            if difference_hour == 1:
+                return str(difference_hour) + ' hora atrás'
             else:
-                return str(diference_hour) + ' horas atrás'
-        elif diference_minute != 0:
-            if diference_minute == 1:
-                return str(diference_minute) + ' minuto atrás'
+                return str(difference_hour) + ' horas atrás'
+        elif difference_minute != 0:
+            if difference_minute == 1:
+                return str(difference_minute) + ' minuto atrás'
             else:
-                return str(diference_minute) + ' minutos atrás'
+                return str(difference_minute) + ' minutos atrás'
         else:
-            if diference_second == 1:
-                return str(diference_second) + ' segundo atrás'
+            if difference_second == 1:
+                return str(difference_second) + ' segundo atrás'
             else:
-                return str(diference_second) + ' segundos atrás'
-    elif diference_date.days <= 3:
-        if diference_date.days == 1:
+                return str(difference_second) + ' segundos atrás'
+    elif difference_date.days <= 3:
+        if difference_date.days == 1:
             return 'Ontem'
         else:
-            return str(diference_date.days) + ' dias atrás'
+            return str(difference_date.days) + ' dias atrás'
     else:
         months = {
             1: 'Janeiro',
@@ -60,3 +60,14 @@ def datetime_sao_paulo():
     utc_time =utc_time.replace(tzinfo=pytz.UTC) 
     st_sao_paulo_time=utc_time.astimezone(tz)
     return datetime.utcnow() #st_sao_paulo_time
+
+def get_datetime_7_days(date_before):
+    date_init = datetime(year=datetime_sao_paulo().year, month=datetime_sao_paulo().month, day=datetime_sao_paulo().day)
+    date_before = datetime(year=date_before.year, month=date_before.month, day=date_before.day)
+
+    difference_days = (date_init - date_before)
+
+    if int(difference_days.days) <= 7:
+        return True
+    else:
+        return False
