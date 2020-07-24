9b69d2bf0fd9d1a02f7dcd711f9e8c07488a7c2c
gabriel-pagotto <gabriel.pagotto@icloud.com>
2020-07-24 12:17:25 -0300

    Muda share

--- app/static/javascript/noticeShare.js
+++ app/static/javascript/noticeShare.js
@@ -4,5 +4,5 @@ const whatsapp = document.querySelector('#whatsapp');
 const url = window.location.href;
 
 facebook.href = 'https://www.facebook.com/sharer/sharer.php?u=' + url;
-twitter.href = 'https://twitter.com/intent/tweet?url=' + url + '&text=' + document.title + '&via=maisgamernews';
+twitter.href = 'https://twitter.com/intent/tweet?url=' + url + '&text=' + document.title;
 whatsapp.href = 'https://api.whatsapp.com/send?text=' + url;
