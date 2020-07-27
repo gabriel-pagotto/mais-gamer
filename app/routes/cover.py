2506303cc4f494fb313f94cc69c470ad28a52d34
gabriel-pagotto <gabriel.pagotto@icloud.com>
2020-07-26 23:07:18 -0300

    Muda notice

--- app/routes/cover.py
+++ app/routes/cover.py
@@ -1,15 +1,15 @@
 import os
 from app import app
 from app.models import Posts, Games
-from app.utils.date_time import DatePost, get_datetime_7_days
+from app.utils.date_time import DatePost
 from app.utils.header_games import header_games
 from app.utils.limit_notices import limit_notices
+from app.utils.main_page_posts_control import posts_control
 from flask import render_template, redirect, flash, request, url_for
 
 @app.route('/', methods=['GET'])
 def index():
-    posts = Posts.query.all()
-    if not posts or len(posts) < 4:
+    if not posts_control()[0] or len(posts_control()[0]) < 4:
         return render_template(
             'index.html',
             title = 'Mais Gamer - Notícias para Gamers',
@@ -18,36 +18,15 @@ def index():
             header_games = header_games,
             limit_notices = limit_notices,
         ) 
-    last_notices = []
-    for post in posts:
-        last_notices.append(post)
-    last_notices.reverse()
-    last_notice = last_notices[0]
-    rest_notices = [
-        last_notices[1],
-        last_notices[2],
-        last_notices[3],
-        last_notices[4],
-        last_notices[5],
-        last_notices[6],
-    ]
 
     games = Games.query.order_by('name').all()
-
-    del(last_notices[0])
-    del(last_notices[0])
-    del(last_notices[0])
-    del(last_notices[0])
-    del(last_notices[0])
-    del(last_notices[0])
-    del(last_notices[0])
 
     return render_template(
         'index.html',
         title = 'Mais Gamer - Notícias sobre Games e eSports',
-        last_notice = last_notice,
-        rest_notices = rest_notices,
-        last_notices = last_notices,
+        last_notice = posts_control()[1],
+        last_notices = posts_control()[2],
+        rest_notices = posts_control()[3],
         games = games,
         DatePost = DatePost,
         header_games = header_games,
