b237e3c16631de67cf200df6b4b48b27b031d875
gabriel-pagotto <gabriel.pagotto@icloud.com>
2020-07-29 23:30:32 -0300

    Adiciona Coluna no model User

--- app/models/User.py
+++ app/models/User.py
@@ -9,6 +9,7 @@ class Users(UserMixin, database.Model):
     surname = database.Column(database.String(60))
     email = database.Column(database.String(120), unique=True)
     password = database.Column(database.String(128))
+    icon = database.Column(database.Text)
     createdAt = database.Column(database.DateTime, default=datetime_sao_paulo)
     is_admin = database.Column(database.Boolean, default=False)
     is_poster = database.Column(database.Boolean, default=False)
