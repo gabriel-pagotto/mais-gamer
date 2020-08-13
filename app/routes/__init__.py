7c87eaf8e9c6c607edbe0306bf5f1a0cdf411786
gabriel-pagotto <gabriel.pagotto@icloud.com>
2020-08-13 20:00:43 -0300

    Retira https redirect

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,1 @@
-from app import app
-from flask import request, redirect
 from app.routes import admin, ajax, auth, cover, notices, posts, user, search
-
-@app.before_request
-def before_request():
-  if not request.is_secure and app.config['DEBUG'] != True:
-    url = request.url.replace("http://", "https://", 1)
-    code = 301
-    return redirect(url, code=code)
