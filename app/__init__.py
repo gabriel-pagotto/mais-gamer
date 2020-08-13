97ca614d7ac9b438a3397a31e1fba9c1ab5ffca1
gabriel-pagotto <gabriel.pagotto@icloud.com>
2020-08-13 19:41:38 -0300

    Adiciona SSLify

--- app/__init__.py
+++ app/__init__.py
@@ -5,19 +5,15 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_sslify import SSLify
 
+
+
 app = Flask(__name__)
+sslify = SSLify(app)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config.from_object(Config)
 database = SQLAlchemy(app)
 migrate = Migrate(app, database)
 login = LoginManager(app)
 login.login_view = 'login'
-
-@app.before_request
-def before_request():
-    if not request.is_secure and app.config['DEBUG'] != True:
-        url = request.url.replace("http://", "https://", 1)
-        code = 301
-        return redirect(url, code=code)
 
 from app import routes, models
