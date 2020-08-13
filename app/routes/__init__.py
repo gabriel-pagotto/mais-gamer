50bc6c16d97ca4bd2e8352ef2226273add65c8da
gabriel-pagotto <gabriel.pagotto@icloud.com>
2020-08-13 19:51:35 -0300

    Adiciona função para dar https redirect

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,11 @@
+from app import app
+from flask import request, redirect
 from app.routes import admin, ajax, auth, cover, notices, posts, user, search
+
+@app.before_request
+def before_request():
+  if app.config['FLASK_DEBUG'] != True:
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
