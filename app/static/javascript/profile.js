2abfc6a7aacd2b62aeee658eabde71b38020b96d
gabriel-pagotto <gabriel.pagotto@icloud.com>
2020-09-26 02:02:52 -0300

    Muda estrutura da index

--- app/static/javascript/profile.js
+++
@@ -1,9 +0,0 @@
-const inputs = document.getElementsByClassName('info-input');
-
-function setInputsDisabled() {
-    for (let counter = 0; counter < inputs.length; counter++) {
-        inputs[counter].disabled = true;
-    };
-};
-
-setInputsDisabled()
